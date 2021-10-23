0b9092413a1cf81c3ea1106eea416b1d02823051
sakkke <w32w64@gmail.com>
2021-10-23 16:00:41 +0900

    add sw.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
 import App from './App.svelte'
 
+if ('serviceWorker' in navigator) {
+  void navigator.serviceWorker.register(new URL('./sw.ts', import.meta.url))
+}
+
 export default new App({ target: document.getElementById('app') as HTMLElement })
