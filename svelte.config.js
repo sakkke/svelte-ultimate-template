f2057c6e5390df11146f8f6c0c3b3924ed47a4eb
sakkke <w32w64@gmail.com>
2021-10-25 16:58:33 +0900

    update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,17 @@
+const postcss = require('postcss')
+const postcssrc = require('postcss-load-config')
 const preprocess = require('svelte-preprocess')
 
 module.exports = {
   preprocess: preprocess({
-    postcss: true
+    async postcss ({ content }) {
+      const { default: stripIndent } = await import('strip-indent')
+      const css = stripIndent(content)
+      if (/{/.test(css)) return { code: css }
+      const { plugins, options } = postcssrc.sync()
+      const { parser } = options
+      const { css: code } = await postcss(plugins).process(css, { parser })
+      return { code }
+    }
   })
 }
