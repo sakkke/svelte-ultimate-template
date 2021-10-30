37198fa51dfdf75dfd789bb7983ede4396031abd
sakkke <w32w64@gmail.com>
2021-10-30 16:01:18 +0900

    update Home.svelte

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -2,8 +2,10 @@
   import Counter from '../components/Counter.svelte'
 
   const name: string = 'svelte-ultimate-template'
+  const src = new URL('../assets/svelte-typescript-sugarss.svg', import.meta.url).toString()
 </script>
 
+<img {src} alt="svelte-typescript-sugarss">
 <h1>Hello {name}</h1>
 <div>
   <Counter></Counter>
